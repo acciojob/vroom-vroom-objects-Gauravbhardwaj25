04775d7652650a0d0115032ce39c89da7253e7a8
Gauravbhardwaj25 <118906941+Gauravbhardwaj25@users.noreply.github.com>
2025-07-17 23:14:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,13 +20,7 @@ SportsCar.prototype.constructor = SportsCar;
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-
-console.log(car.getMakeModel()); 
-console.log(car.getTopSpeed());  
-
-console.log(car instanceof SportsCar); 
-console.log(car instanceof Car);       
+ 
 
 
 
